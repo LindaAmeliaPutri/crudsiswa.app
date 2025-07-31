0d398135192d9a8c8714bb2ba753bd387f1f612d
LindaAmeliaPutri <lindaamelia20328@gmail.com>
2025-07-31 12:02:34 +0700

    style/faet/fix(siswa):create view siswa

---
+++ resources/views/siswa/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Siswa</title>
+</head>
+<body>
+    <h1>halaman Data siswa</h1>
+    <p>Data siswa jurusan pplg</p>
+    <a href="/siswa/create">Tambah data</a>
+    <table border ="1px">
+            <thead>
+                <tr>
+                    <th>Nama</th>
+                    <th>Nisn</th>
+                    <th>Emain</th>
+                    <th>Alamat</th>
+                    <th>Opsi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Amelia</td>
+                    <td>123</td>
+                    <td>Amelia@gmail.com</td>
+                    <td>indonesia</td>
+                    <td>
+                        <a href="">Edit</a>
+                        <a href="">Detail</a>
+                        <a href="">Delet</a>
+                    </td>
+                </tr>
+            </tbody>
+    </table>
+</body>
+</html>
